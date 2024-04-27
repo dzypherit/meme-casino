5449d266be0e76bd85d96a8c6124737ed383b293
dzypherit <31358319+dzypherit@users.noreply.github.com>
2024-04-27 21:10:31 +0800

    Update WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -103,20 +103,20 @@ export function WelcomeBanner() {
   return (
     <Welcome>
       <div>
-        <h1>Welcome to Gamba v2 👋</h1>
+        <h1>Welcome to Casino of Memes v2 👋</h1>
         <p>
           A fair, simple and decentralized casino on Solana.
         </p>
       </div>
       <Buttons>
-        <button onClick={() => window.open(EXPLORER_URL, '_blank')}>
-          🚀 Add Liquidity
+        <button onClick={() => window.open('#', '_blank')}>
+          💬 Telegram Community
         </button>
-        <button onClick={() => window.open('https://github.com/gamba-labs/gamba', '_blank')}>
-          👨‍💻 Build your own
+        <button onClick={() => window.open('#', '_blank')}>
+          🐦 Twitter
         </button>
-        <button onClick={() => window.open('https://discord.gg/HSTtFFwR', '_blank')}>
-          💬 Discord
+        <button onClick={() => window.open('#', '_blank')}>
+          🛒 Dex
         </button>
       </Buttons>
     </Welcome>
